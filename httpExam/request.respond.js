ba43d957d1ff2b5aa8fe8ddf2c54c7f49e7c0aee
kimyoungjae <babjul314@ajou.ac.kr>
2017-09-23 07:39:09 +0000

    EJS,JADE 추가

--- httpExam/request.respond.js
+++ httpExam/request.respond.js
@@ -1,17 +1,47 @@
 var http = require("http");
 var fs = require("fs");
+var ejs = require("ejs");
+var jade = require("jade");
 http.createServer(function(req,res){
+
+ 
     if(req.method=='GET'){
-        console.log(req.url+" GET");
-        fs.readFile("index.html", function(err,data){
-            if(!err){
-                res.writeHead(200,{'Content-Type':'text/html'});
-                res.end(data);
-            } else {
-                res.writeHead(404);
-                res.end();
+        
+        
+           if(req.url == '/'){
+              console.log(req.url+" GET");
+              fs.readFile("index.html", function(err,data){
+                if(!err){
+                         res.writeHead(200,{'Content-Type':'text/html'});
+                         res.end(data);
+                } else {
+                         res.writeHead(404);
+                         res.end();
             }
         });
+                }else if (req.url == '/ejs')
+                {
+                    fs.readFile('template.ejs','utf8',function (err,data){
+                     if(!err){
+                         var html = ejs.render(data,{name:'hong',description: 'hello,world for ejs'});
+                         res.writeHead(200,{'Content-type':'text/html'});
+                         res.end(html);
+                }
+                        
+                    })        
+                }else if(req.url =='/jade')
+                {
+                    fs.readFile('template.jade','utf8',function(err,data){
+                        if(!err){
+                            var fn = jade.compile(data);
+                            var html = fn({name:'hong',description: 'hello,world for ejs'});
+                           res.writeHead(200,{'Content-type':'text/html'});
+                            res.end(html); 
+                        }
+                        
+                    });
+                }
+     
     }else if(req.method =='POST'){
         console.log(req.url+ " Post");
        
